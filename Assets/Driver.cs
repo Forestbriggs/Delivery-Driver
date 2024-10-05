648d077990547c95ca3f03e6dcd10d58abf36313
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-10-05 12:26:58 -0700

    implemented speed ups and slow downs

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UIElements;
 
 public class Driver : MonoBehaviour
@@ -8,17 +9,45 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] float steerSpeed = 300;
     [SerializeField] float moveSpeed = 20;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+    bool hasSpedUp;
+    bool hasSlowedDown;
 
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-
+        if (other.tag == "Speed Up")
+        {
+            hasSlowedDown = false;
+            hasSpedUp = true;
+        }
     }
 
-    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        hasSpedUp = false;
+        hasSlowedDown = true;
+    }
+
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float moveAmount;
+
+        if (hasSpedUp)
+        {
+            moveAmount = Input.GetAxis("Vertical") * boostSpeed * Time.deltaTime;
+        }
+        else if (hasSlowedDown)
+        {
+
+            moveAmount = Input.GetAxis("Vertical") * slowSpeed * Time.deltaTime;
+        }
+        else
+        {
+            moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        }
+
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
